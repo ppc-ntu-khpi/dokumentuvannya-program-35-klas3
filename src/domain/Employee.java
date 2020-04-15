b8495d1601488afde4c6792a3442f1ff507b1e60
Sir.Leviathan <sluvkann@gmail.com>
2020-04-15 09:48:23 +0300

    Add javadoc documentation

--- src/domain/Employee.java
+++ src/domain/Employee.java
@@ -1,6 +1,14 @@
 package domain;
-public class Employee {
 
+/**
+ * Represents an employee of the company
+ * @author Ivan Shapovalov
+ */
+public class Employee {
+    /**
+     * Method that returns full information about the employee
+     * @return full employee information
+     */
     @Override
     public String toString() {
         return "\nEmployee ID= " + ID + "\nName= " + name + "\nJobTitle= " + jobTitle + "\nLevel= " + level + "\nDept= " + dept;
@@ -14,6 +22,13 @@ public class Employee {
 
    // private static int employeesCount = 0;
 
+    /**
+     * Constructor with full employee's information
+     * @param name name of the employee
+     * @param jobTitle job title of the employee
+     * @param level level of the employee
+     * @param dept department of the employee
+     */
     public Employee(String name, String jobTitle, int level, String dept) {
         this();
         setName(name);
@@ -30,6 +45,9 @@ public class Employee {
         this.dept = dept;
     }
 
+    /**
+     * Default constructor of the employee
+     */
     public Employee() {
         ID = (int)(Math.random()*1000);
 //        if (ID > 10000) {
@@ -37,18 +55,34 @@ public class Employee {
 //        }
     }
 
+    /**
+     * Method that assign job title to the employee
+     * @param job job title of the employee
+     */
     public void setJobTitle(String job) {
         jobTitle = job;
     }
 
+    /**
+     * Method that returns job title of the employee
+     * @return job title of the employee
+     */
     public String getJobTitle() {
         return jobTitle;
     }
 
+    /**
+     * Method that returns name of the employee
+     * @return name of the employee
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method that assign level to the employee
+     * @param level level of the employee
+     */
     public void setLevel(int level) {
         switch (level) {
             case 1:
@@ -61,18 +95,34 @@ public class Employee {
         }
     }
 
+    /**
+     * Method that returns level of the employee
+     * @return level of the employee
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Method that returns department of the employee
+     * @return department of the employee
+     */
     public String getDept() {
         return dept;
     }
 
+    /**
+     * Method that assign department to the employee
+     * @param dept department of the employee
+     */
     public void setDept(String dept) {
         this.dept = dept;
     }
 
+    /**
+     * Method that assign name to the employee
+     * @param name name of the employee
+     */
     public void setName(String name) {
 //        boolean correctName = true;
 //        for (int i = 0; i < name.length(); i++) {
--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,8 @@
 package domain;
+
+/**
+ * Represents an graphic illustrator of the company
+ * @author Ivan Shapovalov
+ */
 public class GraphicIllustrator extends Artist {
 }
